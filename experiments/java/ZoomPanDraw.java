2755f22ec531e690ecc0f39fcc0269303f39a201
Bob Kuczewski <bobkuczewski@salk.edu>
2018-02-26 20:20:02 -0800

    Added a file chooser dialog box to allow opening a file.
    So far this tool is fairly generic. It supports opening any file type
    that Java recognizes. It supports drawing onto that file. At some point,
    the special "stack of images" features of reconstruct will make this
    program more specialized.

--- experiments/java/ZoomPanDraw.java
+++ experiments/java/ZoomPanDraw.java
@@ -9,6 +9,21 @@ import java.io.*;
 import java.awt.image.*;
 import javax.imageio.ImageIO;
 
+
+class MyFileChooser extends JFileChooser {
+  MyFileChooser ( String path ) {
+    super(path);
+  }
+  protected JDialog createDialog(Component parent) throws HeadlessException {
+    JDialog dialog = super.createDialog(parent);
+    dialog.setLocation(300, 200);
+    dialog.setSize ( 1024, 768 );
+    // dialog.setResizable(false);
+    return dialog;
+  }
+}
+
+
 public class ZoomPanDraw extends ZoomPanLib implements ActionListener, MouseMotionListener {
 
 	static int w=800, h=600;
@@ -16,6 +31,8 @@ public class ZoomPanDraw extends ZoomPanLib implements ActionListener, MouseMoti
 	boolean drawing_mode = false;
 	boolean stroke_started = false;
   BufferedImage image_frame = null;
+  String current_directory = "";
+  MyFileChooser file_chooser = null;
 
 	ArrayList strokes = new ArrayList();  // Argument (if any) specifies initial capacity (default 10)
 	ArrayList stroke  = null;
@@ -199,51 +216,93 @@ public class ZoomPanDraw extends ZoomPanLib implements ActionListener, MouseMoti
 	    strokes = new ArrayList();
 	    stroke  = null;
    	  repaint();
+		} else if (cmd.equalsIgnoreCase("Add")) {
+			System.out.println ( "Opening new file ..." );
+			// String old_path = current_base_path;
+
+		  // MyFileChooser file_chooser = new MyFileChooser ( this.current_directory );
+		  //FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+		  // FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images", "jpg", "gif");
+		  // file_chooser.setFileFilter(filter);
+		  // data_set_chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		  int returnVal = file_chooser.showDialog(this, "Image File to Add");
+		  if ( returnVal == JFileChooser.APPROVE_OPTION ) {
+        System.out.println ( "You chose to open this file: " /* + chooser.getCurrentDirectory() + " / " */ + file_chooser.getSelectedFile() );
+        String file_path_and_name = "?Unknown?";
+        try {
+          file_path_and_name = "" + file_chooser.getSelectedFile();
+          File image_file = new File ( file_path_and_name );
+          this.image_frame = ImageIO.read(image_file);
+	        repaint();
+        } catch (Exception oe) {
+	        this.image_frame = null;
+	        JOptionPane.showMessageDialog(null, "File error for: " + file_path_and_name, "File Path Error", JOptionPane.WARNING_MESSAGE);
+	        repaint();
+        }
+		  }
 		} else if (cmd.equalsIgnoreCase("Exit")) {
 			System.exit ( 0 );
 		}
   }
 
 	public static void main ( String[] args ) {
+	  for (int i=0; i<args.length; i++) {
+		  System.out.println ( "Arg[" + i + "] = \"" + args[i] + "\"" );
+		}
 		System.out.println ( "Use the mouse wheel to zoom, and drag to pan." );
 		javax.swing.SwingUtilities.invokeLater ( new Runnable() {
 			public void run() {
 			  JFrame f = new JFrame("Zoom/Pan Drawing Demonstration");
 				f.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
 				
-				ZoomPanDraw zp = new ZoomPanDraw();
+        ZoomPanDraw zp = new ZoomPanDraw();
+        /* Can't use args in here
+        if (args.length > 0) {
+          zp.current_directory = args[0];
+        } else {
+	        zp.current_directory = System.getProperty("user.dir");
+        }
+        */
+        zp.current_directory = System.getProperty("user.dir");
 
 				JMenuBar menu_bar = new JMenuBar();
           JMenuItem mi;
           ButtonGroup bg;
           JMenu file_menu = new JMenu("File");
-	        	file_menu.add ( mi = new JMenuItem("New") );
-	        	mi.addActionListener(zp);
-	        	file_menu.add ( mi = new JMenuItem("Exit") );
-	        	mi.addActionListener(zp);
-	      	  menu_bar.add ( file_menu );
+            file_menu.add ( mi = new JMenuItem("Next") );
+            mi.addActionListener(zp);
+            file_menu.add ( mi = new JMenuItem("Previous") );
+            mi.addActionListener(zp);
+            file_menu.add ( mi = new JMenuItem("Add") );
+            mi.addActionListener(zp);
+            file_menu.add ( mi = new JMenuItem("Remove") );
+            mi.addActionListener(zp);
+            file_menu.add ( mi = new JMenuItem("Exit") );
+            mi.addActionListener(zp);
+            menu_bar.add ( file_menu );
           JMenu mode_menu = new JMenu("Mode");
             bg = new ButtonGroup();
-	        	mode_menu.add ( mi = new JRadioButtonMenuItem("Move", true) );
-	        	mi.addActionListener(zp);
-	        	bg.add ( mi );
-	        	mode_menu.add ( mi = new JRadioButtonMenuItem("Draw") );
-	        	mi.addActionListener(zp);
-	        	bg.add ( mi );
-	        	mode_menu.add ( mi = new JMenuItem("Dump") );
-	        	mi.addActionListener(zp);
-	        	mode_menu.add ( mi = new JMenuItem("Clear") );
-	        	mi.addActionListener(zp);
-	      	  menu_bar.add ( mode_menu );
+            mode_menu.add ( mi = new JRadioButtonMenuItem("Move", true) );
+            mi.addActionListener(zp);
+            bg.add ( mi );
+            mode_menu.add ( mi = new JRadioButtonMenuItem("Draw") );
+            mi.addActionListener(zp);
+            bg.add ( mi );
+            mode_menu.add ( mi = new JMenuItem("Dump") );
+            mi.addActionListener(zp);
+            mode_menu.add ( mi = new JMenuItem("Clear") );
+            mi.addActionListener(zp);
+            menu_bar.add ( mode_menu );
 				f.setJMenuBar ( menu_bar );
 				
 				zp.setBackground ( new Color (0,0,0) );
+		    zp.file_chooser = new MyFileChooser ( zp.current_directory );
 
         try {
 			    zp.image_frame = ImageIO.read(new File("test.png"));
 			    // int type = zp.image_frame.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : zp.image_frame.getType();
 		    } catch (IOException e) {
-			    System.out.println( "\n\nI/O Error opening JPEG input file.\n" + e + "\n" );
+			    System.out.println( "\n\nCannot open default \"test.png\" file:\n" + e + "\n" );
 		    }
 
 				f.add ( zp );
