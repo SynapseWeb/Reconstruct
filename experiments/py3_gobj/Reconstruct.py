362927b4d560a0af46ec5ddc65f62cd8349426ca
Bob Kuczewski <bobkuczewski@salk.edu>
2018-03-30 23:38:16 -0700

    Slightly modified example from the tutorial
    Tutorial page:
    
      https://python-gtk-3-tutorial.readthedocs.io/en/latest/introduction.html

--- experiments/py3_gobj/Reconstruct.py
+++ experiments/py3_gobj/Reconstruct.py
@@ -1,9 +1,8 @@
 import gi
-gi.require_version("Gtk", "3.0")
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-window = Gtk.Window(title="Hello World")
-window.show()
-window.connect("destroy", Gtk.main_quit)
+win = Gtk.Window()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
 Gtk.main()
-
